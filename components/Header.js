9295d857b04947ad5633ae4c893263d44f28bd39
llovellusadev <llovellusadev@gmail.com>
2019-01-12 19:48:08 -0600

    mobile content

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,18 @@
+import MediaQuery from 'react-responsive';
+
 class Header extends React.Component {
   render() {
     return (
       <header style={headerStyle}>
-        <div>IMS Managed Services</div>
+        <MediaQuery maxDeviceWidth={1224}>
+          {(matches) => {
+            if (matches) {
+              return <div style={mobileStyle}>IMS Managed Services</div>;
+            } else {
+              return <div style={desktopStyle}>IMS Managed Services</div>;
+            }
+          }}
+        </MediaQuery>
       </header>
     );
   }
@@ -12,7 +22,14 @@ const headerStyle = {
   flex: '1',
   margin: '0px',
   padding: '15px',
-  fontFamily: 'Quicksand',
+  fontFamily: 'Quicksand'
+};
+
+const mobileStyle = {
+  fontSize: '10em'
+};
+
+const desktopStyle = {
   fontSize: '2em'
 };
 
